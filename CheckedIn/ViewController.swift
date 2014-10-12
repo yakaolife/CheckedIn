8b6ccdf29faa44c6c515f38cea2bbc6fb83c22ea
Ishmeet Singh <ishmsing@gmail.com>
2014-10-12 02:27:16 -0700

    Initial Commit

---
+++ CheckedIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckedIn
+//
+//  Created by Ishmeet Singh on 10/12/14.
+//  Copyright (c) 2014 Group6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
