267bbb9d639c3d5579b76b9269ec9779fd5ff2b6
Lakshmi <lakshmi.reguna@yahoo.com>
2014-10-19 15:00:15 -0700

    Test commit by Lakshmi

---
+++ CheckedIn/removeMe.swift
@@ -0,0 +1,11 @@
+//
+//  removeMe.swift
+//  CheckedIn
+//
+//  Created by lareguna on 10/19/14.
+//  Copyright (c) 2014 Group6. All rights reserved.
+//
+
+import Foundation
+
+//Test commit by lakshmi
