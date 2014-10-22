ca3cd87d6b141cb88568e2221c74f698574e3bf9
yakaolife <yakaolife@gmail.com>
2014-10-22 10:16:00 -0700

    Add App-wide UI customization colors

--- CheckedIn/AppDelegate.swift
+++ CheckedIn/AppDelegate.swift
@@ -13,10 +13,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        customizeUI()
+
         return true
+    }
+    
+    //App-wide UI customization (i.e. Navigation bar color, button color, etc
+    func customizeUI(){
+        
+        //Navigation title font R:203 G:249 B:134
+        let titleColorDictionary: NSDictionary = [NSForegroundColorAttributeName:UIColor(red: 203/255, green: 249/255, blue: 134/255, alpha: 1)]
+        UINavigationBar.appearance().titleTextAttributes = titleColorDictionary
+        //Navigation item font R:255 G:193 B:126
+        UINavigationBar.appearance().tintColor = UIColor(red: 255/255, green: 193/255, blue: 126/255, alpha: 1)
+        //Navigation bar R:108 G:122 B:137
+        UINavigationBar.appearance().barTintColor = UIColor(red: 108/255, green: 122/255, blue: 137/255, alpha: 1)
+        //Regular buttons background R:63 G:195 B:168
+        UIButton.appearance().tintColor = UIColor.whiteColor()
+        UIButton.appearance().backgroundColor = UIColor(red: 63/255, green: 195/255, blue: 168/255, alpha: 1)
+
+        
     }
 
     func applicationWillResignActive(application: UIApplication) {
