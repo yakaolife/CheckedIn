2fcd42366ba8336ae3d0bdac634fbc9d0ff7d378
yakaolife <yakaolife@gmail.com>
2014-10-24 17:30:30 -0700

    Add User Profile page

---
+++ CheckedIn/View Controllers/UserProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UserProfileViewController.swift
+//  CheckedIn
+//
+//  Created by Ya Kao on 10/24/14.
+//  Copyright (c) 2014 Group6. All rights reserved.
+//
+
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
