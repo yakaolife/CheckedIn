c9b9c1e7a740c70f939818a76b1e5740b71b060a
blky <jademails@hotmail.com>
2014-10-27 23:10:39 -0700

    model name change

---
+++ CheckedIn/Models/ParseEvent.swift
@@ -0,0 +1,64 @@
+//
+//  ParseEvent.swift
+//  CheckedIn
+//
+//  Created by Cindy Zheng on 10/27/14.
+//  Copyright (c) 2014 Group6. All rights reserved.
+//
+
+import UIKit
+
+class ParseEvent: PFObject,PFSubclassing {
+    
+    override class func load() {
+        self.registerSubclass()
+    }
+    
+    class  func  parseClassName() -> String! {
+        return "Event"
+    }
+    override class func query() -> PFQuery! {
+        return PFQuery(className: "Event")
+    }
+    
+    
+    var EventName:String? {
+        get {return objectForKey("EventName") as String? }
+        set {setObject(newValue, forKey: "EventName") }
+    }
+    var streetAddress:String? {
+        get {return objectForKey("streetAddress") as String?}
+        set {setObject(newValue, forKey: "streetAddress")}
+    }
+    var cityName:String? {
+        get {return objectForKey("cityName") as String?}
+        set {setObject(newValue, forKey: "cityName")}
+    }
+    var state:String? {
+        get {return objectForKey("State") as String?}
+        set {setObject(newValue, forKey: "State")}
+    }
+    var tagLine:String? {
+        get {return objectForKey("tagLine") as String?}
+        set {setObject(newValue, forKey: "tagLine")}
+    }
+    var rsvpMax:NSNumber? {
+        get {return objectForKey("rsvpMax") as NSNumber?}
+        set {setObject(newValue, forKey: "rsvpMax")}
+    }
+    var rsvpCount:NSNumber? {
+        get {return objectForKey("rsvpCount") as NSNumber?}
+        set {setObject(newValue, forKey: "rsvpCount")}
+    }
+    var eventDate:NSDate? {
+        get {return objectForKey("EventDate") as NSDate?}
+        set {setObject(newValue, forKey: "EventDate")}
+    }
+    var eventProfileImage:PFFile?  {
+        get {return objectForKey("eventProfileImage") as? PFFile}
+        set {setObject(newValue, forKey: "eventProfileImage")}
+    }
+    
+    
+    
+}
