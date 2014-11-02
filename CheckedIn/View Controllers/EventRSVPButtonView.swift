d69f4feac573347a02649860ba0a798d0c57974b
yakaolife <yakaolife@gmail.com>
2014-11-02 14:48:14 -0800

    RSVP button

---
+++ CheckedIn/View Controllers/EventRSVPButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  EventRSVPButtonView.swift
+//  CheckedIn
+//
+//  Created by Ya Kao on 11/2/14.
+//  Copyright (c) 2014 Group6. All rights reserved.
+//
+
+import UIKit
+
+class EventRSVPButtonView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
